85ac1162f834ea9872cd56b08a06d95e37708991
Fábio Cavadas <fabiocfferreira@gmail>
2021-11-20 16:59:47 -0300

    Incluido função e tela de autenticação

--- core/views.py
+++ core/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
 # def index(request):
 #         return redirect('/agenda/')
 
+def login_user(request):
+        return render(request, 'login.html')
+
+def logout_user(request):
+        logout(request)
+        return redirect('/')
+
+def submit_login(request):
+        if request.POST:
+                username = request.POST.get('username')
+                password = request.POST.get('password')
+                usuario = authenticate(username=username, password=password)
+                if usuario is not None:
+                        login(request,usuario)
+                        return redirect('/')
+                else:
+                        messages.error(request,"Usuário ou senha inválidos")
+        return redirect('/')
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
+        usuario = request.user
+        evento = Evento.objects.filter(usuario=usuario) # retornará as agendas do usuário logado!!
         #evento = Evento.objects.get(id=1) - Retorna evento de um usuário específico
-        evento = Evento.objects.all() # Retorna eventos de todos os usuários
+        #evento = Evento.objects.all() # Retorna eventos de todos os usuários
         #usuario = request.user # Retorna eventos do usuário logado na sessão
         #evento = Evento.objects.filter(usuario=usuario)
         dados = {'eventos':evento}
